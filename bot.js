a133e575398dc6f5b566bda6ff1de3ecab41e207
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-02 18:37:31 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const fs = require("fs");
+
+const client = new Discord.Client();
+const config = require("./config.json");
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+client.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+client.login("NTA2MjU0MDU0MzM5Mzc5MjIw.Dr49Yg.ziX9opltqvw_w4q6MXnHMx6DAt0");
---
+++ commands/ping.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("pong!").catch(console.error);
+}
