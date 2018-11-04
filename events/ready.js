c7bb9b2c8c37a96b1ae96cb3a10cf8a5fdd4c5ef
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-04 09:50:29 -0500

    Delete ready.js

--- events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = (client) => {
-    console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
-    client.user.setActivity('over my lovelies uwu', { type: 'WATCHING' });
-}
