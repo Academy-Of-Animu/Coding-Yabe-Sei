e2cad12ec918fb564c65efb60aa4efb4efe80b8d
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-06 12:50:14 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
-// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
 fs.readdir("./events/", (err, files) => {
