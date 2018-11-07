da7dd863a61eda08a851ac8a659d39bc36acb3a1
IllusionMan1212 <zyxunzack@gmail.com>
2018-11-08 00:32:51 +0200

    DM user when kicked

--- commands/kick.js
+++ commands/kick.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 exports.run = (client, message, [mention, ...reason]) => {
     const modRole = message.guild.roles.find(role => role.name === "Mods");
     if (!modRole)
@@ -9,7 +11,23 @@ exports.run = (client, message, [mention, ...reason]) => {
     if (!message.guild.me.hasPermission("KICK_MEMBERS"))
       return message.reply("I need permissions to kick!");
     const kickMember = message.mentions.members.first();
-    kickMember.kick(reason.join(" ")).then(member => {
+    if (reason != "") {
+      const embed = new Discord.RichEmbed()
+      .setTitle("Kicked from server")
+      .setDescription(`${kickMember.guild}`)
+      .addField("Reason for kick", `${reason.join(" ")}`)
+      .setColor(client.config.embedColor)
+      kickMember.send(embed);
+    }
+    else {
+      const embed = new Discord.RichEmbed()
+      .setTitle("Kicked from server")
+      .setDescription(`${kickMember.guild}`)
+      .addField("Reason for kick", "No reason was specified")
+      .setColor(client.config.embedColor)
+      kickMember.send(embed);
+    }
+    setTimeout(() => kickMember.kick().then(member => {
       message.reply(`${member.user.username} was succesfully kicked.`);
-    });
+    }), 500);
 }
