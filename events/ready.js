e48d0656a5426a2491bfac6af4612af8444cc32a
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-23 00:37:49 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,13 @@
 module.exports = (client) => {
+    //const activitiesList = [`coding-yabe-sei.io`, `for ${client.users.size} users on ${client.guilds.size} servers`, `with the >help command`, `with the devs`]
+
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
-    client.user.setActivity('over my lovelies uwu', { type: 'WATCHING' });
+    /*
+    setInterval( function() {
+        const index = Math.floor(Math.random() * (activitiesList -1) +1);
+        client.user.setActivity(activitiesList[index]);
+    }, 20000)
+    */
+
+    client.user.setActivity(`>help | ${client.guilds.size} servers`, { type: 'PLAYING' });
 }
