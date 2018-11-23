924d527f00477625dffae5b4d88d738ec21e5a70
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-23 00:23:18 -0500

    Delete rule.js

--- rule.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js')
-
-exports.run = (client, message, args) => {
-    message.channel.send('Command coming soon!')
-}
-
-//going to allow mods to send an embed for cleaner looking rules and info templates
