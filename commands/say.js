cb8ffc4d37b08f07fadee2bc063120602a62a265
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-23 00:26:00 -0500

    Delete say.js

--- commands/say.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, message, args) => {
-    if(message.member.roles.find(x => x.name === "Mods")) {
-        let text = args.join(" ");
-        message.delete();
-        message.channel.send(text);
-    } else {
-        message.channel.send("Sorry, you can't use this command. Maybe ask your the leader for a promotion?");
-    }
-}
