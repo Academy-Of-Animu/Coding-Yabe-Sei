006e5f884a4f3706e8872bd831b4219ab488ece1
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-24 18:22:03 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = (client, message) => {
     let embed = new Discord.RichEmbed()
-    .setTitle(`Changelog V${client.config.botVersion}`)
+    .setTitle(`Changelog v.${client.config.botVersion}`)
     .setDescription("- Fixed reason for kick command\n- Removed the need for a `Mods` role\n- New changelog command(this!!)\n- Small version bump")
     .setFooter("Last updated on 25th/Nov/2018")
     .setColor(client.config.embedColor)
