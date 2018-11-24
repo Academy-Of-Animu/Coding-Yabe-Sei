d986ccaa7078b13fbfe5601818f4756978a6990b
Adam K <43103068+canarado@users.noreply.github.com>
2018-11-24 12:33:47 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
     let embed = new Discord.RichEmbed()
     .setColor(client.config.embedColor)
-    .setDescription(`:clock1: ${message.createdTimestamp - Date.now()}ms\n\n:heart: ${Math.round(client.ping)}ms`)
+    .setDescription(`:clock1: ${Date.now() - message.createdTimestamp}ms\n\n:heart: ${Math.round(client.ping)}ms`)
 
     message.channel.send(embed)
 }
