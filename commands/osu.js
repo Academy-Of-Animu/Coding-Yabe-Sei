0daaea4725662f6e5777216f3f5b88d3828e0154
Adam K <43103068+canarado@users.noreply.github.com>
2018-12-06 13:07:21 -0500

    Update osu.js

--- commands/osu.js
+++ commands/osu.js
@@ -4,7 +4,6 @@ const osu = require('node-osu');
 var osuApi = new osu.Api(process.env.OSU_TOKEN);
 
 exports.run = (client, message, args) => {
-    try {
     osuApi.getUser({u: args.join(' ')}).then(user => {
         //console.log(user)
         const embed = new Discord.RichEmbed()
@@ -21,9 +20,6 @@ exports.run = (client, message, args) => {
             .addField("Rank S Plays", user.counts.S, true)
             .addField("Rank SS Plays", user.counts.SS, true);
         message.channel.send(embed);
-    } catch(err) {
-        console.log(`Error in Osu command: ${err}`)
-    }
 })}
 
 exports.help = {
