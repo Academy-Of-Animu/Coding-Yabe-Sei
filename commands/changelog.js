922c9643e7e7f71e98794f7ff9998f8522a7b51d
Adam K <43103068+canarado@users.noreply.github.com>
2018-12-05 19:36:09 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 exports.run = (client, message) => {
     let embed = new Discord.RichEmbed()
     .setTitle(`Changelog ${client.config.botVersion}`)
-    .setDescription("- Changed prefix from `>` to `yabe` to resolve conflicts\n- Added a `translate` command\n- Rewrote the entirety of the help command\n- Version bump")
+    .setDescription("- Changed prefix from `>` to `yabe` to resolve conflicts\n- Added a `translate` command\n- Rewrote the entirety of the help command\n- Added Osu and Uptime commands\n- Version bump")
     .addField("Previous Changelogs", "For all changelogs, join my [support server](https://discord.gg/bhZGHCm)")
-    .setFooter("Last updated on 30th/Nov/2018")
+    .setFooter("Last updated on 5th/Dec/2018")
     .setColor(client.config.embedColor)
     
     message.channel.send(embed);
