9838b52ff7ffe14bbe1b87419a5eaa6575759e42
Adam K <43103068+canarado@users.noreply.github.com>
2018-12-05 22:20:09 -0500

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,6 +2,10 @@ exports.run = (client, message, args) => {
     try {
     let roleSearch = args.join(" ")
     let roleToAdd = message.guild.roles.find(x => x.name === roleSearch)
+    
+    if(!roleSearch) return message.reply('please provide a role for me to add.')
+    if(message.member.roles.has(roleToAdd.id)) return message.reply('you already have that role!')
+    if(!roleToAdd) return message.reply(`That role, **${roleSearch}**, does not exist.`)
 
     message.member.addRole(`${roleToAdd.id}`).then(() => message.react(`✅`)).catch(console.error)
 
@@ -9,7 +13,7 @@ exports.run = (client, message, args) => {
         message.delete();
     }, 5000 )
     } catch (err) {
-        console.log(`some role couldnt be added`)
+        console.log('There was an error: ' + err)
     }
 }
 exports.help = {
