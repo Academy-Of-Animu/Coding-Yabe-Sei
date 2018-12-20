8e68fce64bebaefff651d4704be31a9a3b090680
Adam K <43103068+canarado@users.noreply.github.com>
2018-12-20 14:15:08 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -5,7 +5,7 @@ exports.run = (client, message) => {
     .setTitle(`Changelog ${client.config.botVersion}`)
     .setDescription("- Added a `send` command\n- Added a `channel` command\n- Version bump")
     .addField("Previous Changelogs", "For all changelogs, join my [support server](https://discord.gg/bhZGHCm)")
-    .setFooter("Last updated on 10th/Dec/2018")
+    .setFooter("Last updated on 20th/Dec/2018")
     .setColor(client.config.embedColor)
     
     message.channel.send(embed);
