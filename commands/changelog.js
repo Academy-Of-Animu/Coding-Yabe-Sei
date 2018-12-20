c54d637a852c0111f8bfe7c1d0e4bd46cb6f0f9b
Adam K <43103068+canarado@users.noreply.github.com>
2018-12-20 14:09:30 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = (client, message) => {
     let embed = new Discord.RichEmbed()
     .setTitle(`Changelog ${client.config.botVersion}`)
-    .setDescription("- Added a roll dice command and a roles display command\n- Added a `translate` command\n- Added Osu and Uptime commands\n- Version bump")
+    .setDescription("- Added a `send` command\n- Added a `channel` command\n- Version bump")
     .addField("Previous Changelogs", "For all changelogs, join my [support server](https://discord.gg/bhZGHCm)")
     .setFooter("Last updated on 10th/Dec/2018")
     .setColor(client.config.embedColor)
