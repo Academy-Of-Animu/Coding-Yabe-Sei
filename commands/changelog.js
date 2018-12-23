257fa074817fbf5ff2a8f02e8ec13d115ad39136
Adam K <43103068+canarado@users.noreply.github.com>
2018-12-22 19:24:50 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 exports.run = (client, message) => {
     let embed = new Discord.RichEmbed()
     .setTitle(`Changelog ${client.config.botVersion}`)
-    .setDescription("- Added a `send` command\n- Added a `channel` command\n- Version bump")
+    .setDescription("- Added a `send` command\n- Added a `channel` command\n- List of languages now available for translate command, use `yabe translate list`\n- Version bump")
     .addField("Previous Changelogs", "For all changelogs, join my [support server](https://discord.gg/bhZGHCm)")
-    .setFooter("Last updated on 20th/Dec/2018")
+    .setFooter("Last updated on 22th/Dec/2018")
     .setColor(client.config.embedColor)
     
     message.channel.send(embed);
