5a11d0ed27199f89f3baf899db9bac8ef9971722
Adam K <43103068+canarado@users.noreply.github.com>
2019-01-06 21:05:01 -0500

    big error uwu also fuck mobile still
    I use Arch btw

---
+++ events/disconnect.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+
+module.exports = (client) => {
+
+  const helps = ['(๑◕︵◕๑) Dads, HELP ME', 'I\'m so scared right now (╯︵╰,)', 'I need to be manually put back UP PLS-DESU UWU', 'Something made a fucky wucky~', '（┬┬＿┬┬）']
+  const helpsR = helps[Math.floor(Math.random() * helps.length)]
+  
+  let embed = new Discord.RichEmbed()
+  .setTitle(helpsR)
+  .setTimestamp()
+  .setFooter('Whoever is seeing this, please check the heroku, thx')
+  .setColor(client.config.embedColor)
+
+  client.channels.get('531654018909339650').send(embed)
+}
