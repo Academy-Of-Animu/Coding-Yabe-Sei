1a9a3b53499c161fdc086ebe83f00b9e8d92b775
Zenzakru <JohnRohnson@outlook.com>
2019-01-10 08:23:22 +0200

    Improved the addrole & roles commands - Zen
    Improved the addrole & roles commands | The role command will now only display roles below the Coding Yabe Sei role, nor will it display Administrator roles - I've also added a blacklist and made selecting a role case-insensitive | The addrole command will now provide feedback on failure - I've removed deleting the command message as that makes me big annoyed
    
    This is my first contribution to my daughter-in-law Yabe <3

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,23 +1,40 @@
 exports.run = (client, message, args) => {
     try {
-    let roleSearch = args.join(" ")
-    let roleToAdd = message.guild.roles.find(x => x.name === roleSearch)
+    let roleSearch = args.join(" ").toLowerCase();
+    let roleToAdd = message.guild.roles.find(x => x.name.toLowerCase() === roleSearch)
     
-    if(!roleSearch) return message.reply('please provide a role for me to add.')
-    if(message.member.roles.has(roleToAdd.id)) return message.reply('you already have that role!')
-    if(!roleToAdd) return message.reply(`That role, **${roleSearch}**, does not exist.`)
+   
 
-    message.member.addRole(`${roleToAdd.id}`).then(() => message.react(`✅`)).catch(console.error)
+    if(!roleSearch) 
+        return message.reply('Gimmie a role ya big silly')
 
-    setTimeout( () => {
-        message.delete();
-    }, 5000 )
+    if(roleToAdd == undefined) 
+        return message.reply(`Unfortunately that role, **${roleSearch}**, does not exist`)
+
+    if(message.member.roles.has(roleToAdd.id)) 
+        return message.reply('You already have that role!');
+    
+
+    message.member.addRole(`${roleToAdd.id}`).then(() => message.react(`✅`))
+    .catch(error => {
+        console.error;
+        message.react('❎');
+        if(error == "DiscordAPIError: Missing Permissions")
+            return message.reply(`You lack the power to gain a role as noble as ${roleToAdd}`);
+        else
+            return message.reply(`Unexpected error | ${error}`)
+    })
+
+  
     } catch (err) {
-        console.log('There was an error: ' + err)
+        console.log('There was an error adding a role: ' + err)
     }
 }
 exports.help = {
     name: "addrole",
-    description: "Allows bot to give you the provided role",
-    usage: "`yabe addrole <role\'s name>(case sensitive)`",
+    description: "Allows yabe to give you the provided role",
+    usage: "`yabe addrole <role\'s name>`",
 }
+
+
+// Improved by best waifu Zen
