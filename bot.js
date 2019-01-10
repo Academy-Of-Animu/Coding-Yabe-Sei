e3ddd067f74d329709fc816792f8388030ca7cf9
Darko Pendragon <darko.pentagon@gmail.com>
2019-01-10 18:33:25 -0500

    added klaw, aliases support, improved command loading

--- bot.js
+++ bot.js
@@ -1,10 +1,14 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
+const klaw = require("klaw");
+const path = require("path");
 
 const client = new Discord.Client();
 const config = require("./config.json");
 client.config = config;
+client.commands = new Enmap();
+client.aliases = new Enmap();
 
 const DBL = require("dblapi.js");
 const dbl = new DBL(process.env.DBL_TOKEN, client);
@@ -17,27 +21,31 @@ dbl.on('error', err => {
  console.log(`${err}`);
 })
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
+const start = async () => {
+  fs.readdir("./events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+      const event = require(`./events/${file}`);
+      let eventName = file.split(".")[0];
+      client.on(eventName, event.bind(null, client));
+    });
   });
-});
 
-client.commands = new Enmap();
-
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    // console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
+  klaw("./commands").on("data", (item) => {
+    const cmdFile = path.parse(item.path);
+    if (!cmdFile.ext || cmdFile.ext !== ".js") return;
+    try {
+      let props = require(`${cmdFile.dir}${path.sep}${cmdFile.name}${cmdFile.ext}`)
+      console.log(`Loading Command: ${props.help.name}${" ".repeat(25 - props.help.name.length)}👌`);
+      client.commands.set(props.help.name, props);
+      if (props.help.aliases) props.help.aliases.forEach(alias => {
+        client.aliases.set(alias, props.help.name);
+      });
+    } catch (e) {
+      return console.log(new Error(`FAIL: ${cmdFile.name}: ${e.stack}`));
+    };
   });
-  console.log('I am prepared to decimate them weebs with my otaku-desu status ÒwÓ')
-});
 
-client.login(process.env.BOT_TOKEN);
+  client.login(process.env.BOT_TOKEN);
+}
+start()
