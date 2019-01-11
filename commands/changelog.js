988609b402d9e8bc56b782513b095a736abfc0d3
MemeSenpai <43326595+MemeSenpai@users.noreply.github.com>
2019-01-10 19:43:57 -0500

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 exports.run = (client, message) => {
     let embed = new Discord.RichEmbed()
     .setTitle(`Changelog ${client.config.botVersion}`)
-    .setDescription("- We got a new way to owo your messages up with `yabe owofy`\n- added the `flip` and `vote` commands.\n- no version bump this time owo")
+    .setDescription("Small update with a big new feature. Added a Pokemon module with `dex` and `move` commands. More to come in the future.")
     .addField("Previous Changelogs", "For all changelogs, join my [support server](https://discord.gg/bhZGHCm)")
-    .setFooter("Last updated on 2nd/Jan/2019")
+    .setFooter("Last updated on 10th/Jan/2019")
     .setColor(client.config.embedColor)
     
     message.channel.send(embed);
