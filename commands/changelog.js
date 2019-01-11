2057d3c20f37b4ea27b3a79df044bdee18d3c4dc
MemeSenpai <43326595+MemeSenpai@users.noreply.github.com>
2019-01-10 19:58:51 -0500

    Version Bump

--- commands/changelog.js
+++ commands/changelog.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = (client, message) => {
     let embed = new Discord.RichEmbed()
     .setTitle(`Changelog ${client.config.botVersion}`)
-    .setDescription("Small update with a big new feature. Added a Pokemon module with `dex` and `move` commands. More to come in the future.")
+    .setDescription("Small update with a big new feature. Added a Pokemon module with `dex` and `move` commands. More to come in the future. Version Bump!")
     .addField("Previous Changelogs", "For all changelogs, join my [support server](https://discord.gg/bhZGHCm)")
     .setFooter("Last updated on 10th/Jan/2019")
     .setColor(client.config.embedColor)
