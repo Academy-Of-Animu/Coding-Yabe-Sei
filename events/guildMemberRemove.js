5a5b704b91f047e2a267cd439345f658a878064e
Darko Pendragon <darko.pentagon@gmail.com>
2019-01-10 19:02:19 -0500

    you dont need that there

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,9 +3,5 @@ module.exports = (client, member) => {
     let homeserver2 = '501540132252286978'
     let welcomechannel1 = '516049633622097931' //support server
     let welcomechannel2 = '501540376998576139'
-    if(member.guild.id === homeserver2) {
-        member.guild.channels.get(welcomechannel2).send('**' + member.user.username + '**, has left the server! RAWR OWO')
-    } else {
-        return
-    }
+    if(member.guild.id === homeserver2) member.guild.channels.get(welcomechannel2).send('**' + member.user.username + '**, has left the server! RAWR OWO')
 }
