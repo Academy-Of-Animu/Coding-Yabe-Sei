f63d816383cb32ccc832188e9ba62c9644b99008
MemeSenpai <43326595+MemeSenpai@users.noreply.github.com>
2019-01-15 19:55:07 -0500

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,6 +3,14 @@ const Discord = require('discord.js')
 module.exports = (client, guild) => {
     try {
     client.user.setActivity(`yabe help | ${client.guilds.size} servers`, { type: 'PLAYING' })
+        
+    let server = client.guild;
+    let bots = 0;
+    let people = 0;
+    server.members.forEach(mem => {
+        if (mem.user.bot){bots += 1;}
+        else{people+=1;}
+    });
 
     let logChannel = client.config.logChannel
 
@@ -10,6 +18,8 @@ module.exports = (client, guild) => {
     .setColor(client.config.embedColor)
     .setTitle(`Just __joined__ ${guild.name}`)
     .setDescription(`**${guild.owner.user.username}#${guild.owner.user.discriminator}** is the owner of the guild.\nGuild has **${guild.members.size}** members.\n\n`)
+    .addField("People: ", people, true);
+    .addField("Bots: ", bots, true)
     .setTimestamp()
 
     client.channels.get(logChannel).send(embed)
