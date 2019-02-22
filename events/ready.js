d5d17e8ec9a9fb0ae7d2f3091130665003bfa92b
IllusionMan1212 <hisham.abourgheba@gmail.com>
2019-02-23 01:01:08 +0200

    refactoring and nitpicks

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,17 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
-module.exports = (client) => {
+module.exports = async (client) => {
     //const activitiesList = [`coding-yabe-sei.io`, `for ${client.users.size} users on ${client.guilds.size} servers`, `with the >help command`, `with the devs`]
+    await client.wait(2000);
+    // Discords API can take upt to 2-5 seconds to be fully ready. This makes sure the bot doesn't fo any of the following code before that.
+    // - Darko
+
+    // You can use this for limitations or fetching the app data in an easy way.
+    // - Also Darko
+    client.appInfo = await client.fetchApplication();
+    setInterval(async () => {
+        client.appInfo = await client.fetchApplication();
+    }, 60000);
 
     console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
     /*
@@ -12,15 +22,15 @@ module.exports = (client) => {
     */
 
     client.user.setActivity(`yabe help | ${client.guilds.size} servers`, { type: 'PLAYING' });
-    
-    const cuties = ['Teehee owo', 'uwu', 'OwO', '\*owo\* notices bulgy wulgy', 'Lovies you papas!~', 'Nyaa! ฅ(＾・ω・＾ฅ)']
-    const cutiesR = cuties[Math.floor(Math.random() * cuties.length)]
-    
-    let embed = new Discord.RichEmbed()
-    .setTitle('I just restarted dads!')
-    .setTimestamp()
-    .setFooter(cutiesR)
-    .setColor(client.config.embedColor)
 
-    client.channels.get('531644078673100830').send(embed)
-}
+    const cuties = ['Teehee owo', 'uwu', 'OwO', '\*owo\* notices bulgy wulgy', 'Lovies you papas!~', 'Nyaa! ฅ(＾・ω・＾ฅ)'];
+    const cutiesR = cuties[Math.floor(Math.random() * cuties.length)];
+
+    let embed = new Discord.RichEmbed()
+        .setTitle('I just restarted dads!')
+        .setTimestamp()
+        .setFooter(cutiesR)
+        .setColor(client.config.embedColor);
+
+    client.channels.get('531644078673100830').send(embed);
+};
