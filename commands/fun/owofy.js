a03ac7c2bdebf8fd6e706d3c912f782d764c31e0
Said Garcia <said.garcia12@gmail.com>
2019-03-20 23:36:30 -0500

    made owofy... better

--- commands/fun/owofy.js
+++ commands/fun/owofy.js
@@ -6,6 +6,14 @@ exports.run = (client, message, args) => {
 
     let newSentence = sentence.replace(/[lr]/g, 'w');
     newSentence = newSentence.replace(/[LR]/g, 'W');
+    newSentence = newSentence.replace(/(te)/g, 'twe');
+    newSentence = newSentence.replace(/(Te)/g, 'Twe');
+    newSentence = newSentence.replace(/(no)/g, 'nyo');
+    newSentence = newSentence.replace(/(No)/g, 'Nyo');
+    newSentence = newSentence.replace(/(ne)/g, 'nye');
+    newSentence = newSentence.replace(/(Ne)/g, 'Nye');
+    newSentence = newSentence.replace(/(love)/g, 'wuv');
+    newSentence = newSentence.replace(/(Love)/g, 'Wuv');
     //more work needs to be done on this *i'm looking at you canny*
     message.channel.send(newSentence);
 }
