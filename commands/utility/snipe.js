0c4f96b5f32be9e295fa265297f50e251f64de89
Adam K <43103068+canarado@users.noreply.github.com>
2019-08-02 19:57:44 -0400

    fix proto

--- commands/utility/snipe.js
+++ commands/utility/snipe.js
@@ -5,6 +5,11 @@ exports.run = (client, message, args) => {
     if(!snipe) return message.channel.send('Could not find a message that was deleted.'.embedify())
 
     return message.channel.send(`Message: ${snipe.content}\n\nAuthor: <@${snipe.author.id}>`.embedify())
+    
+    String.prototype.embedify = function() {
+        return new Discord.RichEmbed().setColor(client.config.embedColor).setDescription(this)
+    }
+
 }
 
 exports.help = {
@@ -14,8 +19,4 @@ exports.help = {
     name: "snipe",
     description: "The `snipe` command allows you to view the last deleted message in the server. Can assist in identifying ghost pingers.",
     usage: "`yabe snipe`"
-}
-
-String.prototype.embedify = function() {
-    return new Discord.RichEmbed().setColor(client.config.embedColor).setDescription(this)
 }
