e711c7b32ce0777b5ae8e0a69bce033db687958a
Adam K <43103068+canarado@users.noreply.github.com>
2019-08-05 16:46:35 -0400

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,3 +1,20 @@
 module.exports = (client, message) => {
-    client.snipeMap.set(message.guild.id, message)
+    if(message && message.attachments && message.attachments.length > 0) return
+
+    if(message.content.length > 200) {
+        message.content = message.content.slice(0, 200) + ' ...'
+    }
+
+    let snipe = client.snipeMap.get(message.guild.id)
+
+    if(!snipe) {
+        client.snipeMap.set(message.guild.id, [message])
+    } else {
+        if(snipe.length >= 5) {
+            client.snipeMap.get(message.guild.id).pop()
+            client.snipeMap.get(message.guild.id).unshift(message)
+        } else {
+            client.snipeMap.get(message.guild.id).unshift(message)
+        }
+    }
 }
