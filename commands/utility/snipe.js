f0e925a83e53bf5322abeac3fd20406f0aec9af5
Adam K <43103068+canarado@users.noreply.github.com>
2019-08-05 16:50:13 -0400

    Update snipe.js

--- commands/utility/snipe.js
+++ commands/utility/snipe.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 exports.run = (client, message, args) => {
     let snipe = client.snipeMap.get(message.guild.id)
-    if(!snipe) return message.channel.send('Could not find a message that was deleted.'.embedify())
+    if(!snipe) return message.channel.send(new Discord.RichEmbed().setDescription('Could not find a message that was deleted.'))
     let counter = 0
 
     return message.channel.send(new Discord.RichEmbed().setDescription(`${snipe.map(msg => `**${++counter} -** ${msg.content}\n**Author -** <@${msg.author.id}>`).join('\n\n')}`).setColor(client.config.embedColor).addField('NOTE:', `Message appear in order, newest deleted message is \`1.\` ,etc. Only the last five deleted messages are preserved. Messages above a 200 character limit are truncated to fit within the embed. Images are not preserved.`))
