57c55f921fd7dc1823ef2f1235751df9f3b5fb32
Adam K <43103068+canarado@users.noreply.github.com>
2019-08-24 00:45:27 -0400

    update snipe to use images

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,8 @@
 module.exports = (client, message) => {
-    if(message && message.attachments && message.attachments.length > 0) return
+
+    let image = message.attachments.size > 0 ? extension(message.attachments.array()[0].proxyURL) : false
+
+    message.content.replace('\n', '')
 
     if(message.content.length > 200) {
         message.content = message.content.slice(0, 200) + ' ...'
@@ -8,13 +11,23 @@ module.exports = (client, message) => {
     let snipe = client.snipeMap.get(message.guild.id)
 
     if(!snipe) {
-        client.snipeMap.set(message.guild.id, [message])
+        client.snipeMap.set(message.guild.id, [[message, image]])
     } else {
         if(snipe.length >= 5) {
             client.snipeMap.get(message.guild.id).pop()
-            client.snipeMap.get(message.guild.id).unshift(message)
+            client.snipeMap.get(message.guild.id).unshift([message, image])
         } else {
-            client.snipeMap.get(message.guild.id).unshift(message)
+            client.snipeMap.get(message.guild.id).unshift([message, image])
         }
+    }
+
+
+
+    function extension(attachment) {
+        const imageLink = attachment.split('.')
+        const typeOfImage = imageLink[imageLink.length - 1]
+        const image = /(jpg|jpeg|png|gif)/gi.test(typeOfImage)
+        if(!image) return ''
+        return attachment
     }
 }
