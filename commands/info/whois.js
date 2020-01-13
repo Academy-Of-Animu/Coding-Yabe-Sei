77bbea9094fbfd6c2e041f7800ca02d2eacc73b3
Sven Fehler <sven.fehler@web.de>
2020-01-13 01:46:26 +0100

    Whois: Made names clickable + always displaying up to 50 users, even if there's more than that (#36)
    * Made names clickable + always displaying up to 50 users, even if there's more than that

--- commands/info/whois.js
+++ commands/info/whois.js
@@ -2,40 +2,44 @@ const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
 	try {
+		let maxUsersToDisplay = 50;
+
 		let roles = message.guild.roles;
 		let roleKeys = roles.keyArray();
 
-		var sendEmbed = (name, role) => {
+		let sendEmbed = (name, role) => {
 			let memberNames = [];
-			let roleMembers = [];
 
 			let roleKeys = role.keyArray()
-			for (key of roleKeys) { memberNames.push(role.get(key)) };
+			roleKeys.forEach(key => memberNames.push(role.get(key)));
 
 			let emb = new Discord.RichEmbed();
 
-			//emb.description = "Who is..." + name;
 			emb.setColor(client.config.embedColor);
 
-			let memberUsers = ""
-			if (memberNames.length > 50) {
-				let embed = new Discord.RichEmbed()
-					.setDescription(`Too many users to display: ${memberNames.length} users.`)
-					.setColor(client.config.embedColor)
+			let memberUsers = "";
+			let moreThanMax = false;
+			let memberCount = memberNames.length;
+			let tooManyText = "";
 
-				message.channel.send(embed)
-				return
-			} //return message.channel.send(`Too many users to display: ${memberNames.length} users`)
-			for (member of memberNames) {
-				memberUsers += member.user.username + "\n"
-			};
+			if (memberNames.length > maxUsersToDisplay)
+			{
+				memberNames.splice(maxUsersToDisplay);
+				moreThanMax = true;
+				tooManyText = " (too many to display)";
+			}
+			
+			memberNames.forEach(member => memberUsers += member.user + "\n");
 
-			emb.addField(`Who is ${name}`, memberUsers);
-			emb.setFooter(`${memberNames.length} users have this role.`)
+			if(moreThanMax)
+				memberUsers += "...";
+
+			emb.addField(`Who is ${name}:`, memberUsers);
+			emb.setFooter(`${memberCount} users have this role${tooManyText}.`)
 			message.channel.send(emb);
 		};
 
-		for (key of roleKeys) {
+		roleKeys.forEach(key => {
 			let role = roles.get(key);
 			let name = role.name;
 			let argS = args.join(' ');
@@ -43,7 +47,7 @@ exports.run = (client, message, args) => {
 				sendEmbed(name, role.members);
 				return;
 			}
-		}
+		});
 	} catch (err) {
 		console.log(err)
 	}
